31ad3951b28de729f8e8938d5b8abcbf7b340468
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 17:16:46 -0300

    Concluido Exercicio 09

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal area;
+            
+            Console.WriteLine("Digite a base do terreno");
+            decimal base1 = Convert.ToDecimal(Console.ReadLine());
+            
+            Console.WriteLine("Digite a altura do terreno");
+            decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+            area = base1 * altura;
+
+            Console.WriteLine($"A area do terreno é {area}");
+            Console.ReadLine();
+            
+
+
+
+        }
+    }
+}
