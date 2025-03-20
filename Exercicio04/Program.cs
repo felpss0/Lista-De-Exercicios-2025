ba02e930ddc53cdfcd7eab0c863df259a2f268fb
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 14:39:49 -0300

    Exercicio 4 concluido

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+
+        //Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+        static void Main(string[] args)
+        {
+            Console.Write("Digite os graus em Celsius: ");
+            decimal celsius = Convert.ToDecimal(Console.ReadLine());
+           
+            decimal fahrenheit =0;
+            decimal resultado = 0;
+            decimal valorApoio = 1.8m;
+            
+
+            resultado = fahrenheit = celsius * valorApoio + 32;
+
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
