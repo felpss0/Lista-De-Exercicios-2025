1b16db295e6325801ed4c15d9dc62b0b81a964f8
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 16:58:30 -0300

    Concluido Exercicio 08

---
+++ Exercicio08/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            bool numeroEhPrimo = true;
+
+            if (numero < 2)
+                numeroEhPrimo = false;
+            else if (numero == 2)
+                numeroEhPrimo = true;
+            else if (numero % 2 == 0)
+                numeroEhPrimo = false;
+
+            for (int i = 3; i * i <= numero; i += 2) 
+            {
+                if (numero % i == 0)
+                    numeroEhPrimo = false;
+            }
+
+            Console.WriteLine(numeroEhPrimo ? "Número primo" : "Não é primo");
+            Console.ReadLine();
+
+        }
+    }
+}
