0ec2969c58ca1e834750e263f1fb516ae94396f7
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 17:56:43 -0300

    Concluido Exercicio 11

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite seu nome");
+            string nome = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Digite sua idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            int diasVividos = idade * 365;
+
+            Console.WriteLine($"{nome}, voce já viveu {diasVividos} Dias.");
+
+
+
+
+        }
+    }
+}
