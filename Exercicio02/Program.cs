3c9d7cdfddda4adbd15bb8f813435eef0c8947de
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 14:28:44 -0300

    Feito os exercicios 1,2 e 3

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio02
+{
+    internal class Program
+    {
+
+        //Crie um programa para calcular o volume de um cilindro
+        //V =  * r² * h
+        static void Main(string[] args)
+        {
+            double raio = 3.1;
+            double altura = 10;
+
+            double volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            Console.WriteLine($"O volume do cilindro é : {volume} cm³");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double kmInicial = 500;
+            double kmFinal = 1000;
+
+            double distanciaPercorrida = kmFinal - kmInicial;
+
+            double consumoCombustivel = 40;
+
+            double consumoPorKm = distanciaPercorrida / consumoCombustivel;
+
+            Console.WriteLine($"O consumo de combustivel durante o percurso foi de {consumoPorKm} KM/l");
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
