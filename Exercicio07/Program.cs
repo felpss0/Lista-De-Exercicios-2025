a31a8de0ab846e518fccfc42a5ba854d9213522e
Filipe Straubel <fstraubel10@gmail.com>
2025-03-20 16:15:59 -0300

    Concluido exercicio 7 sobre media ponderada

--- Exercicio07/Program.cs
+++ Exercicio07/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double resultado;
+
+            Console.WriteLine("Digite a primeira nota:");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro peso");
+            double peso1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota:");
+            double n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo peso:");
+            double peso2 = Convert.ToDouble(Console.ReadLine());
+
+            resultado = ((n1 * peso1) + (n2 * peso2)) / (peso1+peso2);
+
+            Console.WriteLine($"A media ponderada é {resultado}");
+
+
+
         }
     }
 }
