6c253fc79dfa732ee0d28815d80c3e9ce1f2adee
Filipe Straubel <fstraubel10@gmail.com>
2025-03-21 00:18:38 -0300

    Concluido Exercicio 12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal porcentagemAumento = 1.15m;
+            decimal porcentagemDesconto = 0.80m;
+
+            Console.WriteLine("Digite o salario");
+            decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+            decimal salarioPosAumento = salario * porcentagemAumento;
+
+            Console.WriteLine($"Salario depois do aumento {salarioPosAumento}");
+
+            decimal salarioPosImposto = salarioPosAumento * porcentagemDesconto * porcentagemAumento;
+
+            Console.WriteLine($"O salario depois dos descontos fica {salarioPosImposto:F2}");
+
+
+
+
+        }
+    }
+}
