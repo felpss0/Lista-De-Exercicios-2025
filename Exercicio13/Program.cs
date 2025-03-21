e32443f61bd700b287bb7b404d9d7fe85efaeafb
Filipe Straubel <fstraubel10@gmail.com>
2025-03-21 00:28:09 -0300

    Concluido Exercicio 13

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o primeiro valor");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro valor");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            if (a + b < c) 
+            {
+                Console.WriteLine("A soma de A + B é menor que o valor de C");
+            }
+            else
+                Console.WriteLine("A soma de A + B é maior que o valor de C");
+
+            Console.ReadLine();
+
+        }
+    }
+}
